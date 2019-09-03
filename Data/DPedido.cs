cbb1120e403466af52e7ef2697e97e23fa684110
SpaceBear96 <cafu.aguilar@tecsup.edu.pe>
2019-09-02 21:25:13 -0500

    Commit

--- Data/DPedido.cs
+++ Data/DPedido.cs
@@ -33,19 +33,19 @@ namespace Data
                     {
                         pedidos.Add(new Pedido
                         {
-                            IdPedido = reader["IdPedido"] == null ? Convert.ToInt32(reader["IdPedido"]) : 0,
-                            IdCliente = reader["IdCliente"] == null ? Convert.ToString(reader["IdCliente"]) : string.Empty,
-                            IdEmpleado = reader["IdEmpleado"] == null ? Convert.ToInt32(reader["IdEmpleado"]) : 0,
-                            FechaPedido = reader["FechaPedido"] == null ? Convert.ToDateTime(reader["FechaPedido"]) : DateTime.MinValue,
-                            FechaEntrega = reader["FechaEntrega"] == null ? Convert.ToDateTime(reader["FechaEntrega"]) : DateTime.MinValue,
-                            FechaEnvio = reader["FechaEnvio"] == null ? Convert.ToDateTime(reader["FechaEnvio"]) : DateTime.MinValue,
-                            FormaEnvio = reader["FormaEnvio"] == null ? Convert.ToInt32(reader["FormaEnvio"]) : 0,
-                            Cargo = reader["Cargo"] == null ? Convert.ToInt32(reader["Cargo"]) : 0,
-                            Destinatario = reader["Destinatario"] == null ? Convert.ToString(reader["Destinatario"]) : string.Empty,
-                            DireccionDestinatario = reader["DireccionDestinatario"] == null ? Convert.ToString(reader["DireccionDestinatario"]) : string.Empty,
-                            RegionDestinatario = reader["RegionDestinatario"] == null ? Convert.ToString(reader["RegionDestinatario"]) : string.Empty,
-                            CodPostalDestinatario = reader["CodPostalDestinatario"] == null ? Convert.ToString(reader["CodPostalDestinatario"]) : string.Empty,
-                            PaisDestinatario = reader["PaisDestinatario"] == null ? Convert.ToString(reader["PaisDestinatario"]) : string.Empty,
+                            IdPedido = reader["IdPedido"] != null ? Convert.ToInt32(reader["IdPedido"]) : 0,
+                            IdCliente = reader["IdCliente"] != null ? Convert.ToString(reader["IdCliente"]) : string.Empty,
+                            IdEmpleado = reader["IdEmpleado"] != null ? Convert.ToInt32(reader["IdEmpleado"]) : 0,
+                            FechaPedido = reader["FechaPedido"] != DBNull.Value ? Convert.ToDateTime(reader["FechaPedido"]) : DateTime.MinValue,
+                            FechaEntrega = reader["FechaEntrega"] != DBNull.Value ? Convert.ToDateTime(reader["FechaEntrega"]) : DateTime.MinValue,
+                            FechaEnvio = reader["FechaEnvio"] != DBNull.Value ? Convert.ToDateTime(reader["FechaEnvio"]) : DateTime.MinValue,
+                            FormaEnvio = reader["FormaEnvio"] != null ? Convert.ToInt32(reader["FormaEnvio"]) : 0,
+                            Cargo = reader["Cargo"] != null ? Convert.ToInt32(reader["Cargo"]) : 0,
+                            Destinatario = reader["Destinatario"] != null ? Convert.ToString(reader["Destinatario"]) : string.Empty,
+                            DireccionDestinatario = reader["DireccionDestinatario"] != null ? Convert.ToString(reader["DireccionDestinatario"]) : string.Empty,
+                            RegionDestinatario = reader["RegionDestinatario"] != null ? Convert.ToString(reader["RegionDestinatario"]) : string.Empty,
+                            CodPostalDestinatario = reader["CodPostalDestinatario"] != null ? Convert.ToString(reader["CodPostalDestinatario"]) : string.Empty,
+                            PaisDestinatario = reader["PaisDestinatario"] != null ? Convert.ToString(reader["PaisDestinatario"]) : string.Empty,
 
 
                         });
